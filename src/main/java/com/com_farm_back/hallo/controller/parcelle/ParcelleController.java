7e66a2ea90ed21f4c8e619b8e6129d22b373cb16
alicevaliha <alicevaliha@gmail.com>
2024-01-28 00:24:41 +0300

    parcelles

---
+++ src/main/java/com/com_farm_back/hallo/controller/parcelle/ParcelleController.java
@@ -0,0 +1,54 @@
+package com.com_farm_back.hallo.controller.parcelle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.com_farm_back.hallo.model.parcelle.Parcelle;
+import com.com_farm_back.hallo.service.ParcelleService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/parcelles")
+public class ParcelleController {
+
+    private final ParcelleService parcelleService;
+
+    @Autowired
+    public ParcelleController(ParcelleService parcelleService) {
+        this.parcelleService = parcelleService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Parcelle>> getAllParcelles() {
+        List<Parcelle> parcelles = parcelleService.getAllParcelles();
+        return new ResponseEntity<>(parcelles, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Parcelle> getParcelleById(@PathVariable("id") int id) {
+        Optional<Parcelle> parcelle = parcelleService.getParcelleById(id);
+        return parcelle.map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Parcelle> createParcelle(@RequestBody Parcelle parcelle) {
+        Parcelle createdParcelle = parcelleService.saveParcelle(parcelle);
+        return new ResponseEntity<>(createdParcelle, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Parcelle> updateParcelle(@PathVariable("id") int id, @RequestBody Parcelle parcelleDetails) throws Exception{
+        Parcelle updatedParcelle = parcelleService.updateParcelle(id, parcelleDetails);
+        return new ResponseEntity<>(updatedParcelle, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteParcelle(@PathVariable("id") int id) {
+        parcelleService.deleteParcelle(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
---
+++ src/main/java/com/com_farm_back/hallo/service/ParcelleService.java
@@ -0,0 +1,64 @@
+package com.com_farm_back.hallo.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.com_farm_back.hallo.model.parcelle.Parcelle;
+import com.com_farm_back.hallo.repository.ParcelleRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ParcelleService {
+
+    private final ParcelleRepository parcelleRepository;
+
+    @Autowired
+    public ParcelleService(ParcelleRepository parcelleRepository) {
+        this.parcelleRepository = parcelleRepository;
+    }
+
+    public List<Parcelle> getAllParcelles() {
+        return parcelleRepository.findAll();
+    }
+
+    public Optional<Parcelle> getParcelleById(int id) {
+        return parcelleRepository.findById(id);
+    }
+
+    public Parcelle saveParcelle(Parcelle parcelle) {
+        return parcelleRepository.save(parcelle);
+    }
+
+    public Parcelle updateParcelle(int id, Parcelle parcelleDetails) throws Exception{
+        Optional<Parcelle> existingParcelle = parcelleRepository.findById(id);
+        if (existingParcelle.isPresent()) {
+            Parcelle updatedParcelle = existingParcelle.get();
+
+            if(updatedParcelle.getId_terrain()!=0){}
+
+            if(updatedParcelle.getLongueur()!=0)
+            updatedParcelle.setLongueur(parcelleDetails.getLongueur());
+
+            if(updatedParcelle.getLargeur()!=0)
+            updatedParcelle.setLargeur(parcelleDetails.getLargeur());
+
+            if(updatedParcelle.getRendement()!=0)
+            updatedParcelle.setRendement(parcelleDetails.getRendement());
+
+            if(updatedParcelle.getCorbeille()!=0)
+            updatedParcelle.setCorbeille(parcelleDetails.getCorbeille());
+
+            return parcelleRepository.save(updatedParcelle);
+
+        } else {
+            
+            
+            throw new Exception("Parcelle non trouvée avec l'ID : " + id);
+        }
+    }
+
+    public void deleteParcelle(int id) {
+        parcelleRepository.deleteById(id);
+    }
+}
