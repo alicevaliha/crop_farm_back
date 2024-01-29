c9413ee399c0bd763c785afbf1cc96e1a7044ef1
alicevaliha <alicevaliha@gmail.com>
2024-01-30 00:30:36 +0300

    cors

--- src/main/java/com/com_farm_back/hallo/config/CorsConfig.java
+++ src/main/java/com/com_farm_back/hallo/config/CorsConfig.java
@@ -9,10 +9,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOriginPatterns("*") // Autoriser toutes les origines
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
